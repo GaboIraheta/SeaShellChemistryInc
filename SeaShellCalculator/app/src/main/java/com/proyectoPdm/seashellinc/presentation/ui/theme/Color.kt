74a5b3b89c96b31147bad1e148524b37b13328d7
DiegoGarcia271004 <00088023@uca.edu.sv>
2025-05-21 11:26:49 -0600

    :tada: ADD: Se agrega la estructura general del proyecto

---
+++ SeaShellCalculator/app/src/main/java/com/proyectoPdm/seashellinc/presentation/ui/theme/Color.kt
@@ -0,0 +1,19 @@
+package com.proyectoPdm.seashellinc.presentation.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val Background: Color = Color(0xFFEBDDC5)
+val MainBlue: Color = Color(0xFF2E4365)
+val LightDarkBlue: Color = Color(0xFF3E5271)
+val DarkBlue: Color = Color(0xFF2A3445)
+val CitrineBrown: Color = Color(0xFF8A3B08)
+val Buff: Color = Color(0xFFF3D58D)
+val Marigold: Color = Color(0xFFE59D2C)
