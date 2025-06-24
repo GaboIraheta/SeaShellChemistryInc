bf32cab5ffb6ab52955efb5fb357a23b3b92a321
DiegoGarcia271004 <00088023@uca.edu.sv>
2025-06-23 20:37:17 -0600

    ADD: Se agrega la navegación a las calculadoras físicas

--- SeaShellCalculator/app/src/main/java/com/proyectoPdm/seashellinc/presentation/ui/screens/calculatorsPhysicalUnits/PhysicalCalculatorsScreens/PartsPerMillionCalculator.kt
+++ SeaShellCalculator/app/src/main/java/com/proyectoPdm/seashellinc/presentation/ui/screens/calculatorsPhysicalUnits/PhysicalCalculatorsScreens/PartsPerMillionCalculator.kt
@@ -37,6 +37,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 // import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
 import com.proyectoPdm.seashellinc.data.local.model.CalculationResult
 import com.proyectoPdm.seashellinc.data.local.model.ToCalculate
 import com.proyectoPdm.seashellinc.presentation.ui.components.AppButton
@@ -50,9 +51,10 @@ import com.proyectoPdm.seashellinc.presentation.ui.theme.DarkBlue
 import com.proyectoPdm.seashellinc.presentation.ui.theme.LightDarkBlue
 import com.proyectoPdm.seashellinc.presentation.ui.theme.MainBlue
 
-@Preview
+
 @Composable
 fun PartsPerMillionCalculator(
+    navController: NavController,
     viewModel: PhysicalCalculatorViewModel = viewModel()
 ) {
     val navigationBarHeight = WindowInsets.navigationBars.asPaddingValues().calculateBottomPadding()
@@ -126,7 +128,10 @@ fun PartsPerMillionCalculator(
 
             ){
                 Spacer(Modifier.width(50.dp))
-                AppGoBackButton(60.dp){}
+                AppGoBackButton(60.dp){
+                    viewModel.clearAllInputs()
+                    navController.popBackStack()
+                }
             }
 
             Spacer(Modifier.height(16.dp))
