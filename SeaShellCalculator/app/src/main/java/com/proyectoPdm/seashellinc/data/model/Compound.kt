4796cac74d7a6ba68165873caf45a296a1d3ac09
GaboIraheta <00021223@uca.edu.sv>
2025-07-03 19:33:52 -0600

    :white_check_mark: Ultimos cambios y mejoras

--- SeaShellCalculator/app/src/main/java/com/proyectoPdm/seashellinc/data/model/Compound.kt
+++
@@ -1,7 +0,0 @@
-package com.proyectoPdm.seashellinc.data.model
-
-data class Compound (
-    val compoundName : String,
-    val chemicalFormula : String?,
-    val molarMass : Double
-)
--- SeaShellCalculator/app/src/main/java/com/proyectoPdm/seashellinc/data/model/responses/CompoundApiResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.proyectoPdm.seashellinc.data.model.responses
-
-import com.google.gson.annotations.SerializedName
-import com.proyectoPdm.seashellinc.data.model.responses.CompoundResponse
-
-data class CompoundApiResponse(
-    @SerializedName("message") val message : List<CompoundResponse>,
-    @SerializedName("status") val status : String
-)
